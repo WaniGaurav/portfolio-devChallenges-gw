91f4f33f94f876aa5377c1e1a93503003fe1a56d
WaniGaurav <gnwani95@gmail.com>
2023-10-06 20:41:26 +0530

    5th commit

--- components/content.js
+++ components/content.js
@@ -22,6 +22,7 @@ let darkMode = ()=>{
     document.documentElement.style.setProperty("--clr-second","#eee");
     document.documentElement.style.setProperty("--clr-third","#28c1ad");
     document.documentElement.style.setProperty("--clr-skill-cart","#32008b");
+    document.documentElement.style.setProperty("--clr-skill-cart2","#033641");
 }
 // Dark mode End
 
@@ -32,6 +33,7 @@ let lightMode = ()=>{
     document.documentElement.style.setProperty("--clr-second","#828282");
     document.documentElement.style.setProperty("--clr-third","#333333");
     document.documentElement.style.setProperty("--clr-skill-cart","#d3f7f2");
+    document.documentElement.style.setProperty("--clr-skill-cart2","#d3f7f2");
 }
 // Light mode End
 
@@ -61,16 +63,39 @@ let technical =[
 let experiance = [
     {
         logosrc:"./components/images/accenture-logo.png",
+        logoAltTxt:"Accenture logo",
         expDate:"Feb 2020 - Aug 2020",
         roleName:"Web Developer",
         roleDesc:"Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie",
     },
     {
         logosrc:"./components/images/accenture-logo.png",
+        logoAltTxt:"Accenture logo",
         expDate:"Jun 2019 - Jan 2020",
         roleName:"Web Developer",
         roleDesc:"Donec aliquam est dui, vel vestibulum diam sollicitudin id. Quisque feugiat malesuada molestie",
     }
+]
+
+let hobbies = [
+    {
+        hobbyPic:"./components/images/gaming.png",
+        hobbyAltTxt:"playing game on playstation",
+        hobbyName:"Gaming",
+        hobbyDesc:"Quisque feugiat malesuada molestie.",
+    },
+    {
+        hobbyPic:"./components/images/playing-chess.png",
+        hobbyAltTxt:"Playing chess on board",
+        hobbyName:"Playng chess",
+        hobbyDesc:"Quisque feugiat malesuada molestie.",
+    },
+    {
+        hobbyPic:"./components/images/tour.png",
+        hobbyAltTxt:"",
+        hobbyName:"Travelling",
+        hobbyDesc:"Quisque feugiat malesuada molestie.",
+    },
 ]
 
 let trchnologiesCard = ()=>{
@@ -93,9 +118,9 @@ let expCard = ()=>{
 
     experiance.map((expData)=>{
         let divClass = document.createElement("div");
-        divClass.className="exp-wrapper";
+        divClass.classList.add("exp-wrapper","wrapper-design");
         divClass.innerHTML=`<div class="exp-wrp-left">
-        <img loading="lazy" class="exp-logo" src="${expData.logosrc}" alt="Past company logo">
+        <img loading="lazy" class="exp-logo" src="${expData.logosrc}" alt="${expData.logoAltTxt}">
       </div>
       <div class="exp-wrp-right">
         <span class="exp">${expData.expDate}</span>
@@ -107,3 +132,18 @@ let expCard = ()=>{
 }
 
 expCard();
+
+let hobbyCard = ()=>{
+    let hob = document.querySelector(".hobbies");
+
+    hobbies.map((hobData)=>{
+        let divClass = document.createElement("div");
+        divClass.classList.add("hobbies-wrapper","wrapper-design");
+        divClass.innerHTML=`<img loading="lazy" class="hob-img" src="${hobData.hobbyPic}" alt="${hobData.hobbyAltTxt}">
+        <span class="hob-name">${hobData.hobbyName}</span>
+        <span class="hob-desc">${hobData.hobbyDesc}</span>`;
+        hob.appendChild(divClass);
+    })
+}
+
+hobbyCard();
